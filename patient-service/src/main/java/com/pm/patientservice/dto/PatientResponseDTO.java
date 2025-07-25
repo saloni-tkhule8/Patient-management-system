f66112b6bb043c88789630f1268b3cd7ca21381b
saloni-tkhule8 <salonitkhule@gmail.com>
2025-07-25 21:49:53 +0530

    feat: added PatientServiceDTO.java

---
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientResponseDTO.java
@@ -0,0 +1,13 @@
+package com.pm.patientservice.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+public class PatientResponseDTO {
+
+    private String id;
+    private String name;
+    private String email;
+    private String address;
+    private String dateOfBirth;
+
+}
--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -11,7 +11,6 @@ public class PatientService {
     private PatientRepository patientRepository;
 
     public PatientRepository(PatientRepository patientRepository) {
-
         this.patientRepository = patientRepository;
     }
 
