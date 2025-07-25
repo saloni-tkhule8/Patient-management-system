15a3c1226b9e530b820778f6bc20c47f5f552bf8
saloni-tkhule8 <salonitkhule@gmail.com>
2025-07-26 01:15:33 +0530

    map Patient to PatientResponseDTO using PatientMapper

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,5 +1,7 @@
 package com.pm.patientservice.service;
 
+import com.pm.patientservice.mapper.PatientMapper;
+import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
@@ -10,11 +12,16 @@ import java.util.List;
 public class PatientService {
     private PatientRepository patientRepository;
 
-    public PatientRepository(PatientRepository patientRepository) {
+    public PatientService(PatientRepository patientRepository) {
         this.patientRepository = patientRepository;
     }
 
-    public List<PatienResponseDTO> getPatients(){
+    public List<PatientResponseDTO> getPatients(){
         List<Patient> patients= patientRepository.findAll();
+
+        return patients.stream()
+                .map( PatientMapper::toDTO).toList();
+
+       
     }
 }
